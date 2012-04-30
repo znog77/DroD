ca9f085f26e789db0fb65bac00c1d02a880eb4f1
Gonzalo A. de la Vega <gadelavega@gmail.com>
2012-04-30 18:37:00 -0300

    Added scatter plot

--- app/assets/javascripts/plot.js
+++ app/assets/javascripts/plot.js
@@ -75,3 +75,49 @@ function plotLine(data){
 	this.c = brd.create('curve',[x,y],{strokeColor:color[curveCount%color.length]});
 	brd.update();
 }
+
+// --- Scatter (points) plot
+function plotXY(data){
+	var color = ['blue','red','magenta', 'green', 'black','yellow'];
+	var i, x=[], y=[], p=[];
+	var d = JSON.parse(data);
+  var curveCount=0;
+  this.container = brd.objects;
+  this.minX;
+  this.minY;
+  this.maxX;
+  this.maxY;
+  //this.boundingBox=[Math.min(), Math.max(),Math.max(), Math.min() ];
+
+	for(i=0;i<d.length;i++){
+		  x[i] = d[i].x;
+		  y[i] = d[i].y;
+      p[i] = brd.create('point',[x[i],y[i]],{name:''});
+		}
+    //Sweep the board to find curves, if there is none, just set the limits to those of the curve
+  for (var obj in container){
+    var element = obj;
+    if ( container[element].elementClass == 4 ){
+      curveCount++;
+    }
+  }
+
+  minX=Array.min(x)
+  maxX=Array.max(x)
+  minY=Array.min(y)
+  maxY=Array.max(y)
+
+  if (curveCount>0) {
+    this.boundingBox = brd.getBoundingBox();
+    if ( boundingBox [0] > minX ) boundingBox [0]=minX;
+    if ( boundingBox [2] < maxX ) boundingBox [2]=maxX;
+    if ( boundingBox [3] > minY ) boundingBox [3]=minY;
+    if ( boundingBox [1] < maxY ) boundingBox [1]=maxY;
+  } else {
+    this.boundingBox = [minX,maxY,maxX,minY];
+  }
+
+	brd.setBoundingBox(this.boundingBox);
+//	this.c = brd.create('curve',[x,y],{strokeColor:color[curveCount%color.length]});
+	brd.update();
+}
