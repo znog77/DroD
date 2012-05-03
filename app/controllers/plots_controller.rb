be1be58ad728e4a94fa8e4c22afe875793d20b48
gagoar <xeroice@gmail.com>
2012-05-03 14:49:55 -0300

    ahora se puede usar todo lo standard dentro de rails para postgresql la consulta quedo en el framework con comentarios de uso refactorizado el metodo sqm

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -2,26 +2,13 @@ class PlotsController < ApplicationController
   def index
   end
   def sqm
-    puts params[:sqm][:from].to_s,params[:sqm][:to].to_s
-    #binding.pry
-    #@data = Instrument.first.surveys.order(:field_id).group_by(&:field_id).values.map{ |x,y| {:x=> x.value.to_i, :y => y.value}}.to_json
-    #@data = Instrument.first.surveys.where('field_id IN (SELECT field_id FROM surveys WHERE type=\'ValueTime\' AND DATE(value_time)>"'+params[:sqm][:from].to_s+' AND DATE(value_time)<'+params[:sqm][:to].to_s+'")').order(:field_id).group_by(&:field_id).values.map{ |x,y| {:x=> x.value.to_i, :y => y.value}}.to_json
-    timeWindows=['year','month' ,'week','day','hour','minute']
-    count=0
-    begin
-      vals=[]
-      timeWindow = timeWindows[count]
-      count=count+1
-      @conn ||= PG.connect( user: 'gonzalo' , dbname: 'drod_development' )
-      sql_sentence = 'SELECT date_trunc(\''+timeWindow+'\', t),avg(v) FROM (SELECT max(value_flt) as v,max(value_time) as t FROM "surveys" INNER JOIN "fields" ON "surveys"."field_id" = "fields"."id" WHERE "fields"."instrument_id" = 2 GROUP BY field_id) AS xy WHERE DATE(t)>\''+params[:sqm][:from].to_s+'\' AND DATE(t)<\''+params[:sqm][:to].to_s+'\' GROUP BY date_trunc(\''+timeWindow+'\',t) ORDER BY date_trunc(\''+timeWindow+'\',t);'
-      @conn.exec(sql_sentence) do |result|
-        result.each do |row|
-            vals << row.values
-        end
-      end
-    puts count.to_s+' '+vals.count.to_s
-    end while vals.count < 50 && count<6
-      
-    @data = vals.map{|x,y| {:x=>Time.parse(DateTime.parse(x).to_s).to_i, :y=>y.to_f }}.to_json
+    from, to = params[:sqm][:from].to_s, params[:sqm][:to].to_s
+    # habria que ordenar esto de una manera mas logica, como una busqueda binaria(del medio hacia los costados o tener un contador en el modelo para saber cual es el mas problable, y ordenar esto mediante la base asi se ordenaria por la probabilidad mas cercana)
+    ['year','month','week','day','hour','minute'].each do |value|
+      sample = Instrument.find_by_sql("SELECT date_trunc('" + value + "', t) as date,avg(v) as value FROM (SELECT max(value_flt) as v,max(value_time) as t FROM surveys INNER JOIN fields ON surveys.field_id = fields.id WHERE fields.instrument_id = 2 GROUP BY field_id) AS xy WHERE DATE(t)>'2011-02-08' AND DATE(t)<'" + to + "' GROUP BY date_trunc('" + value + "',t) ORDER BY date_trunc('" + value +"',t);")
+      #if you use alias in the resultant data, you can use element.alias to invoke the result value.(ej sample.first.date and sample.first.value ) dont care if the attribute its in the model.
+      @data = sample.map{|x| {:x=>Time.parse(DateTime.parse(x.date).to_s).to_i, :y=>x.value.to_f }}.to_json
+      return unless sample.size < 50
+    end
   end
 end
