41626a296b347d72de03e87d1590471ffa235b89
gagoar <xeroice@gmail.com>
2012-05-06 17:31:52 -0300

    corregido cuando no tenemos datos, mostrar no data available

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -6,14 +6,16 @@ class PlotsController < ApplicationController
     if from <= to
       if (@instrument = Instrument.find_by_id(id))
         scope = @instrument.timeWindow(from,to) # => de aca sacamos la ventana de tiempo en la q deberiamos tomar los datos.
-        binding.pry
         a1 = @instrument.fields.includes(:surveys).where(["surveys.value_time BETWEEN ? AND  ?",from,to])
         a2 = @instrument.fields.includes(:surveys).where(["field_id IN (?) AND surveys.type != ?",a1.map(&:id),"ValueTime"])
         result = (a1 + a2).group_by(&:id).values.map{|f1,f2| {f1.surveys.map{|ff| ff.value.strftime(scope)}.first => f2.surveys.map{|ff| ff.value}}}
-        data = result.inject{|memo, el| memo.merge( el ){|k, old_v, new_v| old_v + new_v}}
-        binding.pry
-        @data=data.keys.map{ |k| {:x => k, :y => data[k].reduce(:+)/data[k].size} }.to_json # => hacemos el promedio por cada key
-        render :text => "range empty" unless @data
+        unless result.empty?
+          data = result.inject{|memo, el| memo.merge( el ){|k, old_v, new_v| old_v + new_v}}
+          #binding.pry
+          @data=data.keys.map{ |k| {:x => k, :y => data[k].reduce(:+)/data[k].size} }.to_json # => hacemos el promedio por cada key
+        else
+          render :text => "no data available"
+        end
       end
     else
       render :text => "worng from and to given"
