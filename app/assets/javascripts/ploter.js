0bffb7295386ca8a5cafbefd28823c21712b11c8
gagoar <xeroice@gmail.com>
2012-05-11 01:29:42 -0300

    agregue exportar el conetenido del grafico directamente en un csv mediante una funcion pura de js (en ie no funciona. pero tira un alert)

--- app/assets/javascripts/ploter.js
+++ app/assets/javascripts/ploter.js
@@ -1,3 +1,30 @@
+function DownloadJSON2CSV(objArray)
+  {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if(line != '') line += ','
+
+            line += array[i][index];
+        }
+
+        str += line + '\r\n';
+    }
+
+    if (navigator.appName != 'Microsoft Internet Explorer')
+    {
+        window.open('data:text/csv;charset=utf-8,' + escape(str));
+    }
+    else
+    {
+        var popup = window.open('','csv','');
+        popup.document.body.innerHTML = '<pre>' + str + '</pre>';
+    }
+}
+
 $(function(){
   if (typeof data !='undefined'){
     brd = plotInit('bbox');
@@ -5,5 +32,7 @@ $(function(){
     xax = xAxis(brd);
     yax = yAxis(brd);
   }
+ $('#download').click(function(){
+   DownloadJSON2CSV(data);
+                     });
 });
-
